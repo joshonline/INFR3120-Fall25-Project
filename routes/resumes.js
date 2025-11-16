0249e4cef2c57de4e25e75bf042d2b8defced16f
joshonline <masanzajoshua@gmail.com>
2025-11-15 19:41:41 -0500

    CRUD routes updated

--- routes/resumes.js
+++ routes/resumes.js
@@ -2,7 +2,25 @@ var express = require("express");
 var router = express.Router();
 var resumeController = require("../controllers/resumeController");
 
-// GET list of resumes
+//CRUD ROUTES
+
+// CREATE
+// GET Request for resume creation
+router.get("/new", resumeController.resume_create_get);
+// POST Request for resume creation
+router.post("/new", resumeController.resume_create_post);
+
+// READ
+// GET request for list of all resumes
 router.get("/", resumeController.resume_catalog);
+// GET one resume
+router.get("/:id", resumeController.resume_detail);
+
+// UPDATE
+router.get("/:id/edit", resumeController.resume_update_get);
+router.put("/:id", resumeController.resume_update_post);
+
+// DELETE
+router.put('/:id/delete', resumeController.resume_delete);
 
 module.exports = router;
