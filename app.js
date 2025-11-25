d90ad8e1546d053c7a0fb08a664e1a6c6ac4ce67
joshonline <masanzajoshua@gmail.com>
2025-11-25 14:51:27 -0500

    Added modules for user auth

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var methodOverride = require('method-override');
+var methodOverride = require("method-override");
 const connectDB = require("./config/database");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var resumesRouter = require("./routes/resumes")
+var resumesRouter = require("./routes/resumes");
+
+const session = require("express-session");
+
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
@@ -22,12 +27,23 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/resumes", resumesRouter)
+app.use("/resumes", resumesRouter);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60000,
+    },
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
