4936b691eed9d3f787d1658fda9317d3c7befdff
joshonline <masanzajoshua@gmail.com>
2025-12-04 11:17:05 -0500

    Updated controllers and routes

--- routes/resumes.js
+++ routes/resumes.js
@@ -1,21 +1,48 @@
-const express = require("express");
+// const express = require("express");
+// const router = express.Router();
+// const resumeController = require("../controllers/resumeController");
+// const { ensureAuth } = require("../middleware/auth");
+
+// // CREATE
+// router.get("/new", ensureAuth, resumeController.resume_create_get);
+// router.post("/new", ensureAuth, resumeController.resume_create_post);
+
+// // READ
+// router.get("/", resumeController.resume_list_get);
+// router.get("/:id", resumeController.resume_detail_get);
+
+// // UPDATE
+// router.get("/:id/edit", ensureAuth, resumeController.resume_update_get);
+// router.put("/:id", ensureAuth, resumeController.resume_update_post);
+
+// // DELETE
+// router.delete("/:id/delete", ensureAuth, resumeController.resume_delete);
+
+// module.exports = router;
+
+
+//------------------------------------------------
+const express = require('express');
 const router = express.Router();
-const resumeController = require("../controllers/resumeController");
-const { ensureAuth } = require("../middleware/auth");
+const resumeController = require('../controllers/resumeController');
+const { requireAuth } = require('../middleware/auth');
 
-// CREATE
-router.get("/new", ensureAuth, resumeController.resume_create_get);
-router.post("/new", ensureAuth, resumeController.resume_create_post);
+// All resume routes require authentication
+router.use(requireAuth);
 
-// READ
-router.get("/", resumeController.resume_list_get);
-router.get("/:id", resumeController.resume_detail_get);
+// GET /api/resumes - List all user's resumes
+router.get('/', resumeController.getResumes);
 
-// UPDATE
-router.get("/:id/edit", ensureAuth, resumeController.resume_update_get);
-router.put("/:id", ensureAuth, resumeController.resume_update_post);
+// POST /api/resumes - Create new resume
+router.post('/', resumeController.createResume);
 
-// DELETE
-router.delete("/:id/delete", ensureAuth, resumeController.resume_delete);
+// GET /api/resumes/:id - Get single resume
+router.get('/:id', resumeController.getResume);
+
+// PUT /api/resumes/:id - Update resume
+router.put('/:id', resumeController.updateResume);
+
+// DELETE /api/resumes/:id - Delete resume
+router.delete('/:id', resumeController.deleteResume);
 
 module.exports = router;
