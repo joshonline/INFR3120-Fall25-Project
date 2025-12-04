5bb8fbd9b037031ba2630ad876f5975900ac0748
1adyan <adyan.iqbal@ontariotechu.net>
2025-12-04 15:42:56 -0500

    did navbar components

--- client/src/app/components/layout/navbar/navbar.ts
+++ client/src/app/components/layout/navbar/navbar.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrl: './navbar.css'
 })
 export class Navbar {
+  isLoggedIn = false;
 
+  constructor(private router: Router) {
+    this.isLoggedIn = !!localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.isLoggedIn = false;
+    this.router.navigate(['/login']);
+  }
 }
