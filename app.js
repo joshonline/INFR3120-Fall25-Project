51ad3027b11b5346aaa9544fcdc884aa9cf82667
joshonline <masanzajoshua@gmail.com>
2025-12-04 09:31:23 -0500

    Depracated old authentication configuration in app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var methodOverride = require("method-override");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const methodOverride = require("method-override");
 const connectDB = require("./config/database");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var resumesRouter = require("./routes/resumes");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const resumesRouter = require("./routes/resumes");
 
-// Import user auth packages
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
-const passport = require("passport");
+// DEPRECATED - Import user auth packages
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
+// const passport = require("passport");
 
 var app = express();
 
 connectDB();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// DEPRECATED - view engine setup
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -30,26 +30,26 @@ app.use(cookieParser());
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
-// ------SESSIONS-------
+// DEPRECATED--SESSIONS-------
 // express-session setup
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI,
-      collectionName: "sessions",
-    }),
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
-    },
-  })
-);
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     store: MongoStore.create({
+//       mongoUrl: process.env.MONGODB_URI,
+//       collectionName: "sessions",
+//     }),
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24,
+//     },
+//   })
+// );
 
-require("./config/passport")(passport);
-app.use(passport.initialize());
-app.use(passport.session());
+// require("./config/passport")(passport);
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 //TASK: Add users/login route, add visit count logic to route
 // TASK: Protect routes
