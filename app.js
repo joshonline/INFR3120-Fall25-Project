360aef3481390b3a1682f220ae25ed61b55b2ce6
joshonline <masanzajoshua@gmail.com>
2025-12-04 10:14:09 -0500

    Cleaned up depracated code from app.js

--- app.js
+++ app.js
@@ -12,18 +12,10 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const resumesRouter = require("./routes/resumes");
 
-// DEPRECATED - Import user auth packages
-// const session = require("express-session");
-// const MongoStore = require("connect-mongo");
-// const passport = require("passport");
 
 var app = express();
 
 connectDB();
-
-// DEPRECATED - view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -31,36 +23,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-
-// DEPRECATED--SESSIONS-------
-// express-session setup
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     store: MongoStore.create({
-//       mongoUrl: process.env.MONGODB_URI,
-//       collectionName: "sessions",
-//     }),
-//     cookie: {
-//       maxAge: 1000 * 60 * 60 * 24,
-//     },
-//   })
-// );
-
-// require("./config/passport")(passport);
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-//TASK: Add users/login route, add visit count logic to route
-// TASK: Protect routes
-// -----------------
-
-// app.use((req, res, next) => {
-//   res.locals.user = req.user ? req.user : null;
-//   next();
-// });
 
 // CORS
 app.use(
@@ -84,19 +46,9 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// DEPRECATED - error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
-
+// error handler
 app.use(function (err, req, res, next) {
-  // render the error page
+  // send error message
   res.status(err.status || 500);
   res.json({
     message: err.message,
