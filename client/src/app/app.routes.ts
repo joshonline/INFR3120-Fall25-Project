78c6a559a03d6de19fd268a36fb68b6fc0a9b7b0
shaheer_deI <shaheer.ahson@ontariotechu.net>
2025-12-04 11:59:22 -0500

    Updated

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
+
 import {Home} from './components/home/home'
 import {Login} from './components/auth/login/login'
+import {Register} from './components/auth/register/register'
+import {ResumeList} from './components/resume/resume-list/resume-list'
+import {ResumeDetail} from './components/resume/resume-detail/resume-detail'
+import {ResumeForm} from './components/resume/resume-form/resume-form'
 
 export const routes: Routes = [
     {path: '',component: Home},
     {path: 'login',component: Login},
+    {path: 'register',component: Register},
+    {path: 'resumes',component: ResumeList},
+    {path: 'resumes/new',component: ResumeDetail},
+    {path: 'resume/:id',component: ResumeForm}
 ];
