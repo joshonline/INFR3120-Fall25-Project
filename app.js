d47c2a9e8f71a2f6a98431f9bc31bf33d7e8f4b6
joshonline <masanzajoshua@gmail.com>
2025-12-04 10:12:18 -0500

    updated routes to use /api prefix, updated error handling to respond with json

--- app.js
+++ app.js
@@ -75,24 +75,33 @@ app.use(
   })
 );
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/resumes", resumesRouter);
+app.use("/api", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/resumes", resumesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// DEPRECATED - error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
 });
 
 module.exports = app;
