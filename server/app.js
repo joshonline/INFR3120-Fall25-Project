01809f810ab10e65b75022a4c77a4fbd00d63506
Joshua Masanza <68029310+joshonline@users.noreply.github.com>
2025-12-06 22:47:02 -0500

    Fix CORS origin URL formatting

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin: [
       // "http://localhost:4200",
-      https://infr3120-fall25-project-yjny.onrender.com/
+      "https://infr3120-fall25-project-yjny.onrender.com/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
