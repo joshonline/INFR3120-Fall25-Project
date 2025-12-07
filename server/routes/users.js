126e0e5c30e2620e75c2aad148885ba32f1ae912
joshonline <masanzajoshua@gmail.com>
2025-12-07 01:07:06 -0500

    cleaned up old code from  controllers, models and routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,37 +1,3 @@
-// var express = require("express");
-// var router = express.Router();
-// const userController = require("../controllers/userController");
-// const { requireAuth, requireGuest } = require("../middleware/auth");
-
-// // GET users/
-// // router.get("/", (req, res) => {
-// //   let visits = req.session.visits;
-// //   if (!visits) {
-// //     visits = 0;
-// //   }
-// //   visits++;
-// //   // res.send(`You have visited ${visits} times`);
-// //   res.redirect('/login')
-// // });
-
-// // REGISTRATION
-// // router.get("/register", userController.register_get);
-// router.post("/register", userController.register_post);
-
-// // LOGIN
-// // router.get("/login", userController.login_get);
-// router.post("/login", userController.login_post);
-
-// // LOGOUT
-// // router.get("/logout", requireAuth, userController.logout);
-// router.post("/logout", requireAuth, userController.logout);
-// router.get("/profile", requireAuth, userController.getProfile);
-// router.put("/profile", requireAuth, userController.updateProfile);
-
-// module.exports = router;
-
-//------------------------------------------------
-
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
