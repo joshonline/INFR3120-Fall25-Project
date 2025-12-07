fd3791cb44fad683b17077392cdf5d7b8ec761ab
Joshua Masanza <68029310+joshonline@users.noreply.github.com>
2025-12-07 00:19:04 -0500

    Update CORS origin for the application

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin: [
       // "http://localhost:4200",
-      "https://infr3120-fall25-project-yjny.onrender.com/"
+      "https://cv-client-infr3120-fall25.onrender.com/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
